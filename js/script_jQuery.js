adb838085074d9a8f3402aad3877a514cac7d356
unknown <johndoe@example.com>
2020-09-23 18:52:45 +0900

    수정

--- js/script_jQuery.js
+++ js/script_jQuery.js
@@ -36,7 +36,52 @@ $(function(){
                 $logo_white.addClass("visible"); // 로고화이트에 visible이라는 클래스명을 추가
             }
         });
-        
+        $(function(){
+            $(".ft_family").click(function(){
+                $(".ft_btn").slideToggle();
+
+            });
+
+        });
+
+        //메인슬라이더 
+        var $slide_wrap = $(".slider");
+        var $slide_container = $(".slide_container");
+        var $slide_list = $(".slider");
+        var $slide_count = $(".slide_container");
+        var $slide_prve = $(".slider");
+        var $slide_next = $(".slide_container");
+        var $current_idx = 0;
+
+        //슬라이드 나열 
+        $slide_list.each(function(){
+            $(this).css("left",i * + "%");
+            i +=0;
+        });
+        //슬라이드 이동 함수  
+        function moveSlide(idx){
+            $slide_container.css("left",-100 * idx + "%");
+        }      
+        //버튼 눌렀을때 할일
+        $btn_next.on("click", function(){
+            if($current_idx == $slide_count -1){
+                moveSlide(0);
+            }else{moveSlide($current_idx +1);
         
 
+        }
+    }); 
+    $btn_prev.on("click", function(){
+        if($current_idx == 0){
+            moveSlide($slide_count -1);
+        }else{
+    
+
+    }
+}); 
+            
+
+       
+
+    
 });
